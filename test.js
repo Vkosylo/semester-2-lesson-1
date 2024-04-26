6e141312edb63f547851871f887698a7d6ff92af
Vkosylo <144223487+Vkosylo@users.noreply.github.com>
2024-04-26 23:08:06 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+function countWords(st) { 
+  return st.trim().split(/\s+/).length;
+} 
+
+const Stream = fs.createReadStream("text.txt");
+let total = 0;
+
+Stream.on('data', (chunk) => {
+  const lines = chunk.toString().split('\n');
+  lines.forEach((line) => {
+    console.log(line);
+    const wordCount = countWords(line);
+    total += wordCount;
+  });
+});
+
+Stream.on('end', () => {
+  console.log(`Total word count: ${total}`);
+  process.exit(0);
+});
+
+Stream.on('error', (err) => {
+  console.error('Error reading the file:', err);
+  process.exit(1);
+});
